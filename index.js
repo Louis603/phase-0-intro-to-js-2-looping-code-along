ea80b4ee4b3230fb93fd8db97bb73722d156a30d
Louis Y <louisy603@hotmail.com>
2022-03-22 16:04:28 -0400

    commit tree e39a5426cdf2c96f2decc5e334f57b6c78925e16

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let newMsgs = []
+//let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, birthday) {
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${birthday} gift!`;
+        newMsgs.push(message);
+    
+    }
+    return newMsgs;
+};
+
+function countDown() {
+    let countdown = 10;
+    while (countdown > -1 ) {
+        console.log(countdown--);
+    }
+};
